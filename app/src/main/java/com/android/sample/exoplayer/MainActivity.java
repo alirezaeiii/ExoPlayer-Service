461a9d863ffdb2dfd447ae2108388b64cf4bc666
Ali Rezaei <ali.rezaei09@gmail.com>
2020-06-20 21:20:22 +0430

    Improvement

--- app/src/main/java/com/android/sample/exoplayer/MainActivity.java
+++ app/src/main/java/com/android/sample/exoplayer/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 mTxtComposer.setText(myService.getSample().getComposer());
                 isPlaying = myService.getExoPlayerInstance().getPlayWhenReady();
                 updateBtnPlayPauseDrawable();
-                if (isPlaying) {
+                if (isPlaying && mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
                     mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
                     mBtnPlayPause.setEnabled(false);
                     mBottomBar.setAlpha(0);
