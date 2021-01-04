287b8af2fbcec3292bfcc7c7938a73d00ee7497f
Ali Rezaei <ali.rezaei09@gmail.com>
2021-01-05 01:07:02 +0330

    Use PublishSubject to allow multiple observer

--- app/src/main/java/com/android/sample/exoplayer/RxBus.java
+++ app/src/main/java/com/android/sample/exoplayer/RxBus.java
@@ -1,22 +1,19 @@
 package com.android.sample.exoplayer;
 
-import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.Subject;
-import io.reactivex.subjects.UnicastSubject;
 
 public final class RxBus {
 
-    //this how to create our bus
-    private static final Subject<Boolean> SUBJECT = UnicastSubject.create();
+    private static final Subject<Boolean> SUBJECT = PublishSubject.create();
 
-    public static Disposable subscribe(@NonNull Consumer<Boolean> action) {
+    public static Disposable subscribe(Consumer<Boolean> action) {
         return SUBJECT.subscribe(action);
     }
 
-    //use this method to send data
-    public static void publish(@NonNull Boolean message) {
+    public static void publish(Boolean message) {
         SUBJECT.onNext(message);
     }
 }
